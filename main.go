6e823249d3a502e8729b3e195dfdf19e81723798
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-09-30 15:23:04 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/exp/rand"
+	"log"
+	"time"
+)
+
+const (
+	// game screen size: pixel
+	screenWidth  = 320
+	screenHeight = 240
+
+	tileSize = 5
+)
+
+func main() {
+	// generate seed values to add game randomness
+	rand.Seed(uint64(time.Now().UnixNano()))
+
+	// init game struct
+	game := &Game{
+		snake:    NewSnake(),
+		food:     NewFood(),
+		gameOver: false,
+		ticks:    0,
+		speed:    10,
+	}
+
+	// set game screen size
+	// `func SetWindowSize(width, height int)`: https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#SetWindowSize
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+
+	// set game screen title
+	// `func SetWindowTitle(title string)`: https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#SetWindowTitle
+	ebiten.SetWindowTitle("Snake Game")
+
+	// start game
+	// `func RunGame(game Game) error`: https://pkg.go.dev/github.com/hajimehoshi/ebiten/v2#RunGame
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
